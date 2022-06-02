b0cf636b7675aa09a5d6a8829e9d715ea77adab8
Lucas alves <lucasryanalves@gmail.com>
2022-06-01 23:57:08 -0300

    Adicionado teste do ipv4

---
+++ Support/ValidationIpv4Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace UnderConstruction.Mock
+{
+    public static class ValidationIpv4Test
+    {
+        public static List<string> EnderecoIpv4
+        {
+            get 
+            { return new List<string>() {
+                "1.2.3",
+                "1.2.3.4.5",
+                "123.456.78.90",
+                "123.045.067.089",
+                "123.45.67.89",
+                "01.02.03.04",
+                "255.255.255.255",
+                "123.194.123.255",
+                "195.299.299.298",
+                "333.333.333.333"
+              };  
+            }
+        }
+    }
+}
