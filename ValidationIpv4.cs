10623653b883e0aa7d0b45b7039aade7638dcc51
Lucas alves <lucasryanalves@gmail.com>
2022-06-02 11:59:11 -0300

    Organizando linha de código na tela de console

--- ValidationIpv4.cs
+++ ValidationIpv4.cs
@@ -7,16 +7,11 @@ using System.Threading.Tasks;
 namespace UnderConstruction
 {
     public class ValidationIpv4 {
-        public static void ValidationIpv4Regex(List<string> endereco)
+        public static List<bool> ValidationIpv4Regex(string endereco)
         {
-            //var result = string.Join("", endereco.Select(i=> ));
 
                 string regex = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-
-           
-                var resultado = endereco.Select(x=> Regex.IsMatch(x, regex)).ToList();
-
-               
+               // return endereco.Select(x=> Regex.IsMatch(x, regex)).ToList();
         }
     }
 }
